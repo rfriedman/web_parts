e9e5a6cbfc5cba1c93c07249f412ad72cb155a44
rfriedman <friedman.richard@gmail.com>
2018-02-09 15:06:11 -0500

    django server files

---
+++ server/myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\rfriedman\documents\github\web_parts\server\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
